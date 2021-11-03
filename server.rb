0161084b4bdb286515881a346ad4048cd3f0f280
Leandro Proença <leandronsp@gmail.com>
2021-11-03 00:17:02 +0000

    implement POST /: append the message to the TXT

--- server.rb
+++ server.rb
@@ -1,20 +1,61 @@
 require 'socket'
+require 'cgi'
 
 socket = TCPServer.new(8080)
 puts "Listening to the port 8080..."
+
+def process_request_and_headers(client)
+  request = ''
+  headers = {}
+
+  while line = client.gets
+    break if line == "\r\n"
+    request += line
+
+    if line.match(/.*?:.*?/)
+      hname, hvalue = line.split(': ')
+      headers[hname] = hvalue.chomp if hvalue
+    end
+  end
+
+  [request, headers]
+end
+
+def process_params(client, headers)
+  params = {}
+
+  if content_length = headers['Content-Length']
+    body = client.read(content_length.to_i)
+    param, value = CGI.unescape(body).split('=')
+    params[param] = value
+  end
+
+  params
+end
 
 loop do
   client        = socket.accept
   first_line    = client.gets
   verb, path, _ = first_line.split(' ')
 
+  request, headers = process_request_and_headers(client)
+  params = process_params(client, headers)
+
   puts first_line
+  puts request
+  puts
 
   case [verb, path]
   in ['GET', '/']
     body     = File.read('./index.html')
     response = "HTTP/1.1 200\r\nContent-Type: text/html\r\n\r\n#{body}"
   in ['POST', '/']
+    timestamp = Time.now.strftime('%d.%B.%Y %H:%M')
+    text      = params['message'].strip
+    message   = "#{timestamp};#{text}\n"
+
+    File.open('./messages.txt', 'a') { |file| file << message }
+
     response = "HTTP/1.1 301\r\nLocation: /\r\n\r\n"
   else
     response = "HTTP/1.1 404\r\nContent-Type: text/html\r\n\r\n<p>Not Found</p>"
@@ -23,3 +64,4 @@ loop do
   client.puts(response)
   client.close
 end
+
